0a10c835e13994c068938a11c1192e1f363eb038
Ariel Steger <ariellekatherine92@gmail.com>
2022-06-21 17:43:50 -0400

    added in coordinate info

--- map1.py
+++ map1.py
@@ -4,13 +4,14 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat=list(data["LAT"])
 lon=list(data["LON"])
+elev = list(data['ELEV'])
 
 map = folium.Map(location=[38.58, -99.09],zoom_start=6,tiles = "Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    map.add_child(folium.Marker(location=[lt, ln], popup="Hi, You are here!", icon=folium.Icon(color='pink')))
+for lt, ln, el in zip(lat, lon, elev):
+    map.add_child(folium.Marker(location=[lt, ln], popup=str(el)+ "m", icon=folium.Icon(color='pink')))
 map.add_child(fg)
 
 
