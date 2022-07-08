64080242e4ad5587d87719062ee4f4b16a06a6b4
Ariel Steger <ariellekatherine92@gmail.com>
2022-07-07 21:43:18 -0400

    added legend and notes

--- map1.py
+++ map1.py
@@ -7,10 +7,12 @@ import pandas
 #layers on map come from overstreet map service - give you layer that folium serves
 
 data = pandas.read_csv("Volcanoes.txt")
-lat=list(data["LAT"])
-lon=list(data["LON"])
-elev = list(data['ELEV'])
+# create lists from csv data to loop through later
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
+# define a color producing function to use later for the circle markers
 def color_producer(elevation):
     if elevation < 1000:
         return 'pink'
@@ -19,35 +21,36 @@ def color_producer(elevation):
     else:
         return 'green'
 
+# instatiate a folium map with an initial location and zoom level
 map = folium.Map(location=[38.58, -99.09],zoom_start=6,tiles = "Stamen Terrain")
-#map object that takes in lat and long cordinates on where to start on the map
-#everything will spin around the folium map object 
 
 #fgv = feature group volcano
 #fgp = feature group population
 
+# create feature groups to add to map later
 fgv = folium.FeatureGroup(name="Volcanoes")
+fgp = folium.FeatureGroup(name="Population")
 
+# looping through volcanos data and storing references for lat, long, and elevation
+# zip three separate lists together and use that combined list for looping
+# add circle marker inside loop with lat/long coordinates
+# feed elevation into color_producer function which returns a cooresponding color value
+# these markers are added to the volcanoes feature group
 for lt, ln, el in zip(lat, lon, elev):
     fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+ "m", 
     fill_color=color_producer(el), color='grey', fill_opacity=0.7))
-
-#used python for loop to create multiple markers on the map  
-
-
-fgp = folium.FeatureGroup(name="Population")
-  
+ 
+# add geo json data from world.json file with style lambda function to set fillColor based on POP2005 column
+# this gets added to the population feature group
 fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), 
 style_function=lambda x: {'fillColor':'yellow' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-#children of map pointing to folium markers which is a built in feature of the folium library
-#adds pop ups and location if you pass in location parameter in the function
-
-
-
+# adding the two feature groups to the map
 map.add_child(fgv)
 map.add_child(fgp)
+# add the layer controls to the map
 map.add_child(folium.LayerControl())
 
-map.save("Map1.html")
+# save the to the Map.html file
+map.save("Map.html")
